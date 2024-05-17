92871765716a9ce5702c0a16f9aa9f61a43ce4c9
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-17 06:02:19 +0600

    dashboard add kazibyte init  by zk

---
+++ src/app/forms/home/page.tsx
@@ -0,0 +1,12 @@
+import CreateHero from '@/components/FormElements/HomeForm/CreateHero'
+import React from 'react'
+
+const CreateHeroPage = () => {
+  return (
+      <div>
+          <CreateHero />
+    </div>
+  )
+}
+
+export default CreateHeroPage
