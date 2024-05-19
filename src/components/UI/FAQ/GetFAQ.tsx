c37bde14a0e3fbf495b0756d71d87c7172cea0ca
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-19 23:49:44 +0600

    FAQ update & delete

---
+++ src/components/UI/FAQ/GetFAQ.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetFAQ = () => {
+  return (
+    <div>GetFAQ</div>
+  )
+}
+
+export default GetFAQ
---
+++ src/components/UI/FAQ/PostFAQ.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostFAQ = () => {
+  return (
+    <div>PostFAQ</div>
+  )
+}
+
+export default PostFAQ
