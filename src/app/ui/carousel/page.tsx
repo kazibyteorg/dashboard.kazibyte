3ec1bf680691efd353ae47cead7a13b2eca53eef
zobkazi <zobaidulkazi64.official@gmail.com>
2024-06-24 16:44:12 +0600

    cras

---
+++ src/app/ui/carousel/page.tsx
@@ -0,0 +1,12 @@
+import Carousel from '@/components/UI/Carousel/Adds'
+import React from 'react'
+
+const CarouselPage = () => {
+  return (
+      <div>
+          <Carousel />
+    </div>
+  )
+}
+
+export default CarouselPage
